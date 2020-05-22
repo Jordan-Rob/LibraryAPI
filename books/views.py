1e0519c85b380378597d7841df9d5fc022aeeff5
jordanrob709 <jordanrob709@gmail.com>
2020-05-22 17:08:44 +0300

    added urls view & template

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.generic.views import ListView, DetailView
+
+from .models import Book
+
+
 # Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'booklist.html'
